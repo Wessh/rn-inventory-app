f57f9496a5a001b04c8ce17ba6894b1856d47a7e
Wesley B.R. <wesshbr@gmail.com>
2025-03-22 14:17:40 -0300

    Botão com clique adicionado e funcional

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App(): JSX.Element {
+
   return (
     <View style={styles.container}>
       <Text>Tudo é uma mentira</Text>
+      <Button 
+      title="Clique aqui" 
+      onPress={() => 
+      alert('Você clicou no botão')
+      } />
       <StatusBar style="auto" />
     </View>
   );
