b596a886c59db1f62b68a09c71734d3463d5225d
Wesley B.R. <wesshbr@gmail.com>
2025-03-22 16:02:02 -0300

    Ajustes nas funções do banco de dados CHECK

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { openDatabase, insertData, closeDatabase, dropUsuariosTable } from './database/simple_db'; // Importando a conexão com o banco de dados
+import { openDatabase, insertData, closeDatabase, dropInventoryTable } from './database/simple_db'; // Importando a conexão com o banco de dados
 
 export default function App(): JSX.Element {
 let nome = 'LaParfun';
@@ -14,8 +14,8 @@ let categoria = 'Perfume';
       title="Clique aqui" 
       onPress={async () => {
         await openDatabase();
-        await insertData(nome, marca, categoria);
-        await dropUsuariosTable();
+        await insertData(nome, marca, categoria, 2);
+        await dropInventoryTable();
         await closeDatabase();
         }} />
       <StatusBar style="auto" />
--- src/database/simple_db.ts
+++ src/database/simple_db.ts
@@ -6,19 +6,24 @@ async function openDatabase() {
   db = await SQLite.openDatabaseAsync('inventory-app');
 
   await db.execAsync(`
-    PRAGMA journal_mode = WAL; CREATE TABLE IF NOT EXISTS inventory (id INTEGER PRIMARY KEY NOT NULL, nome TEXT NOT NULL, marca TEXT, categoria TEXT);`);
+    PRAGMA journal_mode = WAL; 
+    CREATE TABLE IF NOT EXISTS inventory (
+    id INTEGER PRIMARY KEY NOT NULL, 
+    nome TEXT NOT NULL, 
+    marca TEXT, 
+    categoria TEXT,
+    quantidade INTEGER
+    );`);
 }
-
 // Exporta a função openDatabase para ser chamada externamente
 export { openDatabase };
-
 // Exporta a função insertData para ser chamada externamente
-export async function insertData(nome: string, marca: string, categoria: string) {
+export async function insertData(nome: string, marca: string, categoria: string, quantidade: number) {
   if (!db) {
     await openDatabase(); // Garante que o banco de dados esteja aberto
   }
   try {
-    const result = await db.runAsync('INSERT INTO inventory (nome, marca, categoria) VALUES (?, ?, ?)', nome, marca, categoria);
+    const result = await db.runAsync('INSERT INTO inventory (nome, marca, categoria, quantidade) VALUES (?, ?, ?, ?)', nome, marca, categoria, quantidade);
     console.log(result.lastInsertRowId, result.changes);
     const firstRow = await db.getFirstAsync('SELECT * FROM inventory');
     console.log(firstRow);
@@ -26,7 +31,6 @@ export async function insertData(nome: string, marca: string, categoria: string)
     console.error("Failed to insert data", e);
   }
 }
-
 // Exporta a função para fechar o banco de dados
 export async function closeDatabase() {
   if (db) {
@@ -35,8 +39,7 @@ export async function closeDatabase() {
   }
 }
 
-// Exporta a função para limpar a tabela usuarios
-export async function clearUsuariosTable() {
+export async function clearInventoryTable() {
     if (!db) {
       await openDatabase(); // Garante que o banco de dados esteja aberto
     }
@@ -46,9 +49,9 @@ export async function clearUsuariosTable() {
     } catch (e) {
       console.error('Erro ao limpar a tabela inventory:', e);
     }
-  }
+}
 
-  export async function getAllUsuarios() {
+export async function getAllInventory() {
     if (!db) {
       await openDatabase(); // Garante que o banco de dados esteja aberto
     }
@@ -58,10 +61,9 @@ export async function clearUsuariosTable() {
     } catch (e) {
       console.error('Erro ao obter todos os usuários:', e);
     }
-  }
+}
 
-  // Exporta a função para remover a tabela usuarios
-  export async function dropUsuariosTable() {
+export async function dropInventoryTable() {
     if (!db) {
       await openDatabase(); // Garante que o banco de dados esteja aberto
     }
@@ -71,4 +73,103 @@ export async function clearUsuariosTable() {
     } catch (e) {
       console.error('Erro ao remover a tabela inventory:', e);
     }
-  }
+}
+
+export async function getInventoryById(id: number) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.getAllAsync('SELECT * FROM inventory WHERE id = ?', id);
+        console.log(result);
+    } catch (e) {
+        console.error('Erro ao obter o inventário pelo ID:', e);
+    }
+}
+
+export async function getInventoryByNome(nome: string) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.getAllAsync('SELECT * FROM inventory WHERE nome = ?', nome);
+        console.log(result);
+    } catch (e) {
+        console.error('Erro ao obter o inventário pelo nome:', e);
+    }
+}
+
+export async function getInventoryByMarca(marca: string) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.getAllAsync('SELECT * FROM inventory WHERE marca = ?', marca);
+        console.log(result);
+    } catch (e) {
+        console.error('Erro ao obter o inventário pela marca:', e);
+    }
+}
+
+export async function getInventoryByCategoria(categoria: string) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.getAllAsync('SELECT * FROM inventory WHERE categoria = ?', categoria);
+        console.log(result);
+    } catch (e) {
+        console.error('Erro ao obter o inventário pela categoria:', e);
+    }
+}
+
+export async function updateInventory(id: number, nome: string, marca: string, categoria: string, quantidade: number) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.runAsync('UPDATE inventory SET nome = ?, marca = ?, categoria = ?, quantidade = ? WHERE id = ?', nome, marca, categoria, quantidade , id);
+        console.log(result.lastInsertRowId, result.changes);
+    } catch (e) {
+        console.error('Erro ao atualizar o inventário:', e);
+    }
+}
+
+export async function deleteInventory(id: number) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.runAsync('DELETE FROM inventory WHERE id = ?', id);
+        console.log(result.lastInsertRowId, result.changes);
+    } catch (e) {
+        console.error('Erro ao deletar o inventário:', e);
+    }
+}
+
+export async function getInventoryByQuantidade(quantidade: number) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.getAllAsync('SELECT * FROM inventory WHERE quantidade = ?', quantidade);
+        console.log(result);
+    } catch (e) {
+        console.error('Erro ao obter o inventário pela quantidade:', e);
+    }
+}
+
+export async function getInventoryByQuantidadeMaiorQue(quantidade: number) {
+    if (!db) {
+        await openDatabase(); // Garante que o banco de dados esteja aberto
+    }
+    try {
+        const result = await db.getAllAsync('SELECT * FROM inventory WHERE quantidade > ?', quantidade);
+        console.log(result);
+    } catch (e) {
+        console.error('Erro ao obter o inventário pela quantidade maior que:', e);
+    }
+}
+
+
+
